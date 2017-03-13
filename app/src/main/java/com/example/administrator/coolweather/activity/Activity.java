40443f9d2b1ae2c0a956ff1c8b66a64da1c81205
lizhipeng123 <1036886274@qq.com>
2017-03-13 17:19:54 +0800

    20170313测试

---
+++ app/src/main/java/com/example/administrator/coolweather/activity/Activity.java
@@ -0,0 +1,12 @@
+package com.example.administrator.coolweather.activity;
+
+/**
+ * Created by lizhipeng on 2017/3/13.
+ */
+
+public class Activity {
+    private int one;
+    private String two;
+
+
+}
